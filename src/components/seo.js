1b9e742bd1d53dff08f2f23d3a5e38176823e82a
slarsendisney <s.larsendisney@gmail.com>
2020-04-23 15:29:00 +0100

    updated SEO image for projects

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title }) {
+function SEO({ description, lang, meta, title, image }) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -29,6 +29,8 @@ function SEO({ description, lang, meta, title }) {
 
   const url = "https://sld.codes"
 
+  const metaImage = image || "https://ik.imagekit.io/sld/Logo__ok8RRhzzg3.png"
+
   return (
     <Helmet>
       <title>Sam Larsen-Disney | {title}</title>
@@ -38,20 +40,14 @@ function SEO({ description, lang, meta, title }) {
       <meta property="og:url" content={url} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={metaDescription} />
-      <meta
-        property="og:image"
-        content="https://ik.imagekit.io/sld/Logo__ok8RRhzzg3.png"
-      />
+      <meta property="og:image" content={metaImage} />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content="Sam Larsen-Disney" />
       <meta name="twitter:title" content={title} />
       <meta property="twitter:url" content={url} />
       <meta name="twitter:description" content={metaDescription} />
-      <meta
-        property="twitter:image"
-        content="https://ik.imagekit.io/sld/Logo__ok8RRhzzg3.png"
-      />
+      <meta property="twitter:image" content={metaImage} />
       <html lang="en" />
     </Helmet>
   )
--- src/templates/Project.js
+++ src/templates/Project.js
@@ -11,7 +11,11 @@ export default ({
   const { frontmatter, html } = markdownRemark
   return (
     <Layout>
-      <SEO title={frontmatter.title} description={frontmatter.desc} />
+      <SEO
+        title={frontmatter.title}
+        description={frontmatter.desc}
+        image={frontmatter.coverimg.childImageSharp.fluid.src}
+      />
       <div className="is-grey is-light-grey-bg">
         <div className="row container pad-10-t pad-20-b ">
           <div className="col-xs-12 pad-10-lr">
