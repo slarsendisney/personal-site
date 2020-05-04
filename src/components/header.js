4b891c8c36a7ae74918221128bf458ec2d41cdff
slarsendisney <s.larsendisney@gmail.com>
2020-05-04 17:33:50 +0100

    header rework

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@
 
 import { Link } from "gatsby"
 import React, { useRef, useEffect, useState } from "react"
-import { Emojione } from "react-emoji-render"
+import ReactTooltip from "react-tooltip"
 import useDarkMode from "use-dark-mode"
 import Logo from "../images/Logo.svg"
 import LogoDark from "../images/Logo-dark.svg"
@@ -44,7 +44,6 @@ export default ({ location }) => {
   }
 
   return (
-    // <VisibilitySensor onChange={onChange} partialVisibility={true}>
     <div
       className="is-white-bg pad-5 "
       style={{ zIndex: 100 }}
@@ -58,26 +57,49 @@ export default ({ location }) => {
             className=" align-horizontal is-white flex grow-on-hover"
             style={{ textDecoration: "none" }}
           >
-            {location.pathname !== "/cv" ? (
-              <LogoAnimation darkMode={darkMode.value} />
-            ) : (
-              <img
-                src={!darkMode.value ? Logo : LogoDark}
-                alt=""
-                style={{ paddingBottom: 2, height: "21.5px" }}
-              />
-            )}
+            <LogoAnimation darkMode={darkMode.value} />
           </Link>
         </div>
         <div
           className="col-xs-3 flex text-align-right"
           style={{ justifyContent: "flex-end", alignItems: "center" }}
         >
+          {darkMode.value ? (
+            <button
+              type="button"
+              aria-label="light mode"
+              onClick={() => {
+                setMenuOpen(false)
+                delay(darkMode.disable)
+              }}
+              data-tip={`Light Mode`}
+            >
+              <h2
+                className={`las la-adjust link margin-0`}
+                style={{ fontSize: 30 }}
+              ></h2>
+            </button>
+          ) : (
+            <button
+              type="button"
+              aria-label="dark mode"
+              onClick={() => {
+                setMenuOpen(false)
+                delay(darkMode.enable)
+              }}
+              data-tip={`Dark Mode`}
+            >
+              <h2
+                className={`las la-adjust link margin-0`}
+                style={{ fontSize: 30 }}
+              ></h2>
+            </button>
+          )}
           <button type="button" onClick={() => setMenuOpen(!menuOpen)}>
             <h2
               className={`las ${
                 !menuOpen ? "la-bars" : "la-times-circle"
-              } link margin-0`}
+              } link margin-0 margin-2-l`}
               style={{ fontSize: 30 }}
             ></h2>
           </button>
@@ -85,58 +107,13 @@ export default ({ location }) => {
         <div className="col-xs-12">
           <SmoothCollapse expanded={menuOpen} className="">
             <div className="row margin-5-t">
-              <div className="col-xs-12 col-md-2 ">
-                {sections.slice(0, 2).map((item) => (
-                  <NavLinkSmall {...item} key={item.label} />
+              {sections
+                .filter((item) => item.nav)
+                .map((item) => (
+                  <div className="col-xs-6 col-md-3 col-lg-2">
+                    <NavLinkSmall {...item} key={item.label} />
+                  </div>
                 ))}
-              </div>
-
-              <div className="col-xs-12 col-md-2 ">
-                {sections.slice(2, 4).map((item) => (
-                  <NavLinkSmall {...item} key={item.label} />
-                ))}
-              </div>
-              <div className="col-xs-12 col-md-2 ">
-                {sections.slice(4, 6).map((item) => (
-                  <NavLinkSmall {...item} key={item.label} />
-                ))}
-              </div>
-              <div className="col-xs-12 col-md-2 ">
-                <NavLinkSmall
-                  {...{
-                    label: "🔎 Search",
-                    type: "Boilerplates",
-                    url: "/search",
-                  }}
-                />
-                {darkMode.value ? (
-                  <button
-                    type="button"
-                    aria-label="light mode"
-                    onClick={() => {
-                      setMenuOpen(!menuOpen)
-                      delay(darkMode.disable)
-                    }}
-                  >
-                    <h3 className="margin-3-b margin-0-t link-bar pad-1-b  special-header-text">
-                      <Emojione text="☀️" /> Light Mode
-                    </h3>
-                  </button>
-                ) : (
-                  <button
-                    type="button"
-                    aria-label="dark mode"
-                    onClick={() => {
-                      setMenuOpen(!menuOpen)
-                      delay(darkMode.enable)
-                    }}
-                  >
-                    <h3 className="margin-3-b margin-0-t link-bar pad-1-b  special-header-text">
-                      <Emojione text="🌙" /> Dark Mode
-                    </h3>
-                  </button>
-                )}
-              </div>
             </div>
           </SmoothCollapse>
         </div>
