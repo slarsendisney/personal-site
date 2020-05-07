042e5f21c3175d9f47398d60758f8b1dfc52457f
slarsendisney <s.larsendisney@gmail.com>
2020-05-07 12:12:18 +0100

    removed planes

--- src/components/Root/PreferPaper.js
+++ src/components/Root/PreferPaper.js
@@ -3,22 +3,6 @@ import { Link } from "gatsby"
 
 export default ({ darkMode }) => (
   <div className="is-grey is-light-grey-bg" style={{ position: "relative" }}>
-    <img
-      alt="planes"
-      src={
-        !darkMode.value
-          ? "https://ik.imagekit.io/sld/Untitled_Artwork_9_oDFR_CBToKE.gif"
-          : "https://ik.imagekit.io/sld/Untitled_Artwork_10_46sRVmnPyiG.gif"
-      }
-      style={{
-        width: "100%",
-        maxHeight: "100%",
-        objectFit: "cover",
-        position: "absolute",
-        zIndex: 0,
-      }}
-    />
-
     <div
       className="row container-small pad-20-tb"
       style={{ position: "relative", zIndex: 5 }}
