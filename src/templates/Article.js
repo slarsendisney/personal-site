0db450f569dd004635314da2ce09fdac4fb5972f
slarsendisney <s.larsendisney@gmail.com>
2020-05-08 17:23:11 +0100

    made article images load gracefully

--- src/templates/Article.js
+++ src/templates/Article.js
@@ -1,24 +1,42 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import ReactHtmlParser from "react-html-parser"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import ArticleShareOptions from "../components/Articles/ArticleShareOptions"
 import ReadingProgress from "../components/Articles/ReadingProgress"
+import { LazyLoadImage } from "react-lazy-load-image-component"
+import "react-lazy-load-image-component/src/effects/blur.css"
+
+function transform(node) {
+  if (node.type === "tag" && node.name === "img") {
+    console.log(node.attribs.src)
+    return (
+      <LazyLoadImage
+        alt="progressive-image"
+        effect="blur"
+        src={node.attribs.src}
+      />
+    )
+  }
+  if (node.type === "tag" && node.name === "a") {
+    if (node.attribs.href.includes("https://sld.codes")) {
+      return (
+        <Link to={node.attribs.href.replace("https://sld.codes", "")}>
+          {node.children[0].data}
+        </Link>
+      )
+    }
+  }
+}
 
 export default ({ data, location }) => {
   const { feedMediumBlog } = data
   const { title, content, pubDate, fields } = feedMediumBlog
-
-  let imgSrcRegex = /src\s*=\s*"(.+?)"/
-  const found = content.encoded.match(imgSrcRegex)
   const target = React.createRef()
   return (
     <Layout>
-      <SEO
-        title={title}
-        description={fields.excerpt}
-        image={found ? found[1] : undefined}
-      />
+      <SEO title={title} description={fields.excerpt} image={fields.hero_img} />
       <ReadingProgress target={target} />
       <div className="is-grey is-light-grey-bg">
         <div className="row container pad-10-t " ref={target}>
@@ -29,7 +47,7 @@ export default ({ data, location }) => {
           </div>
 
           <div className="col-xs-12 pad-5-lr">
-            <h1 className="is-hero-menu is-pink-always margin-1-t margin-5-b">
+            <h1 className="is-hero-menu is-grey margin-1-t margin-5-b">
               {title}
             </h1>
             <h6 className="is-hero-sub-text margin-3-b">
@@ -39,10 +57,9 @@ export default ({ data, location }) => {
               )}
             </h6>
             <div className="line margin-5-tb" />
-            <div
-              className={`${content.encoded ? "lato article" : ""}`}
-              dangerouslySetInnerHTML={{ __html: content.encoded }}
-            />
+            <div className="lato article">
+              {ReactHtmlParser(content.encoded, { transform })}
+            </div>
           </div>
           <div className="col-xs-12 pad-5-lr pad-5-b">
             <ArticleShareOptions location={location} />
@@ -61,6 +78,7 @@ export const pageQuery = graphql`
       pubDate
       fields {
         excerpt
+        hero_img
       }
       content {
         encoded
