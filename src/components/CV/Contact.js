22a9761112167cbf72948a5d4e0b7a6d7bf50439
slarsendisney <s.larsendisney@gmail.com>
2020-07-01 03:27:57 +0100

    removed number from CV

--- src/components/CV/Contact.js
+++ src/components/CV/Contact.js
@@ -4,7 +4,7 @@ export default ({ contact }) => (
   <h5 className="body-text margin-0" style={{ color: "#067BC2" }}>
     {contact.website}&nbsp;&nbsp;|&nbsp;&nbsp;
     {contact.email}
-    &nbsp;&nbsp;|&nbsp;&nbsp;
-    {contact.phone}
+    {/* &nbsp;&nbsp;|&nbsp;&nbsp;
+    {contact.phone} */}
   </h5>
 )
