025647c75e1dcc3ec9b5c894e778803d3a9e9ba2
slarsendisney <s.larsendisney@gmail.com>
2020-07-14 10:01:28 +0100

    fixing caching for gatsby cloud

--- gatsby-node.js
+++ gatsby-node.js
@@ -172,3 +172,13 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     }
   }
 }
+
+const fs = require("fs")
+
+exports.onPostBuild = () => {
+  fs.copyFile(`./firebase.json`, `./public/firebase.json`, (err) => {
+    if (err) {
+      throw err
+    }
+  })
+}
