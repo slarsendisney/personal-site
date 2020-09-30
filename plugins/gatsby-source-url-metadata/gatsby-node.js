70009b2039a7f6bf1a6f625c2b6b75602a2e48ad
slarsendisney <s.larsendisney@gmail.com>
2020-09-30 19:45:48 +0100

    yo

--- plugins/gatsby-source-url-metadata/gatsby-node.js
+++ plugins/gatsby-source-url-metadata/gatsby-node.js
@@ -4,23 +4,20 @@ const domino = require("domino");
 
 const got = require("got");
 
-exports.sourceNodes = async ({ actions }, configOptions) => {
+function checkCacheValid(cache, urls) {
+  if (cache.every((r) => urls.includes(r))) {
+    return true;
+  }
+  return false;
+}
+
+exports.sourceNodes = async ({ actions, cache }, configOptions) => {
   const { createNode } = actions;
   const { urls } = configOptions;
-  Promise.all(
-    urls.map((suppliedUrl) => {
-      let actualUrl = suppliedUrl;
-      if (!actualUrl.match(/^[a-zA-Z]+:\/\//)) {
-        actualUrl = "http://" + actualUrl;
-      }
-      return got(actualUrl).then(({ body: html, url }) => {
-        console.log(`Retreiving metadata for ${actualUrl}`);
-        const doc = domino.createWindow(html).document;
-        return { ...getMetadata(doc, url), suppliedUrl };
-      });
-    })
-  ).then((metaData) => {
-    metaData.forEach((site) => {
+  const currentCache = await cache.get(`gatsby-source-metadata-urls`);
+  if (currentCache && checkCacheValid(JSON.parse(currentCache).urls, urls)) {
+    console.log("🔥 Using cached metadata for urls");
+    JSON.parse(currentCache).metaData.forEach((site) => {
       createNode({
         ...site,
         id: site.suppliedUrl,
@@ -35,6 +32,46 @@ exports.sourceNodes = async ({ actions }, configOptions) => {
         },
       });
     });
-    return metaData;
-  });
+  } else {
+    Promise.all(
+      urls.map((suppliedUrl) => {
+        let actualUrl = suppliedUrl;
+        if (!actualUrl.match(/^[a-zA-Z]+:\/\//)) {
+          actualUrl = "http://" + actualUrl;
+        }
+        return got(actualUrl).then(({ body: html, url }) => {
+          const doc = domino.createWindow(html).document;
+          return { ...getMetadata(doc, url), suppliedUrl };
+        });
+      })
+    )
+      .then((metaData) => {
+        metaData.forEach((site) => {
+          createNode({
+            ...site,
+            id: site.suppliedUrl,
+            internal: {
+              type: `MetaData`,
+              mediaType: `text/plain`,
+              contentDigest: crypto
+                .createHash(`md5`)
+                .update(JSON.stringify(site))
+                .digest(`hex`),
+              description: `MetaData For URL`,
+            },
+          });
+        });
+        return metaData;
+      })
+      .then((metaData) => {
+        console.log("💾 Caching metadata for urls");
+        cache.set(
+          `gatsby-source-metadata-urls`,
+          JSON.stringify({
+            metaData,
+            urls,
+          })
+        );
+      });
+  }
 };
