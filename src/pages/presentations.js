f48dcdc830886b6263b4b27920103809bfe46ed0
Sam Larsen-Disney <s.larsendisney@gmail.com>
2021-01-17 16:15:36 +0000

    animated gradients always makes things better

--- src/pages/presentations.js
+++ src/pages/presentations.js
@@ -13,32 +13,32 @@ const Start = ({ data, presentation }) => {
       <SEO title="Presentations" socialcard="social-card-presentations" />
       <div className="flex-1 w-full max-w-4xl px-4 py-8 mx-auto md:px-8 md:py-16">
         <h3 className="text-4xl font-semibold mb-1">Presentations</h3>
-
-        <SmoothCollapse expanded={presentation && presentation.deck}>
-          <Link
-            to={
-              presentation
-                ? presentation.deck + "/slides/" + presentation.slide
-                : "/"
-            }
-            style={{ textDecoration: "none" }}
-          >
-            <div className="flex is-pink-bg-always is-white-always margin-5-b margin-3-lr pad-3 border-radius">
-              <h4 className="margin-0">
-                Sam is currently presenting "
-                {presentation &&
-                  nodes.find(
-                    (item) =>
-                      presentation &&
-                      presentation.deck === item.frontmatter.path
-                  ).frontmatter.title}
-                ". Click to follow along!
-              </h4>
-            </div>
-          </Link>
-        </SmoothCollapse>
-
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="md:col-span-2 lg:col-span-3">
+            <SmoothCollapse expanded={presentation && presentation.deck}>
+              <Link
+                to={
+                  presentation
+                    ? presentation.deck + "/slides/" + presentation.slide
+                    : "/"
+                }
+                style={{ textDecoration: "none" }}
+              >
+                <div className="flex multiColourGoodness text-white  mb-3 p-3 rounded">
+                  <p className="font-bold text-xl text-center m-0">
+                    Sam is currently presenting "
+                    {presentation &&
+                      nodes.find(
+                        (item) =>
+                          presentation &&
+                          presentation.deck === item.frontmatter.path
+                      ).frontmatter.title}
+                    ". Click here to follow along!
+                  </p>
+                </div>
+              </Link>
+            </SmoothCollapse>
+          </div>
           {[
             ...nodes.filter(
               (item) =>
@@ -79,7 +79,8 @@ const Start = ({ data, presentation }) => {
                       style={{
                         maxHeight: 200,
                       }}
-                      className="rounded-t" />
+                      className="rounded-t"
+                    />
 
                     <div className="p-3">
                       <h1 className="margin-0-b text-secondary text-2xl">
@@ -99,23 +100,27 @@ const Start = ({ data, presentation }) => {
   );
 };
 
-export const query = graphql`{
-  allMdx(sort: {fields: frontmatter___date, order: DESC}, filter: {frontmatter: {type: {eq: "Presentation"}}}) {
-    nodes {
-      frontmatter {
-        path
-        title
-        desc
-        location
-        hero {
-          childImageSharp {
-            gatsbyImageData(maxWidth: 1000, placeholder: NONE, layout: FLUID)
+export const query = graphql`
+  {
+    allMdx(
+      sort: { fields: frontmatter___date, order: DESC }
+      filter: { frontmatter: { type: { eq: "Presentation" } } }
+    ) {
+      nodes {
+        frontmatter {
+          path
+          title
+          desc
+          location
+          hero {
+            childImageSharp {
+              gatsbyImageData(maxWidth: 1000, placeholder: NONE, layout: FLUID)
+            }
           }
         }
       }
     }
   }
-}
 `;
 
 const mapStateToProps = ({ presentation }) => {
