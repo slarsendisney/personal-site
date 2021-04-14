7edadff02a744f256e9a934c90162fa768c33992
Sam Larsen-Disney <s.larsendisney@gmail.com>
2021-04-14 20:26:57 +0100

    added front-end survival guide deck

---
+++ MDX/Decks/WEBDEVINTRO2/CodeSnippets.js
@@ -0,0 +1,16 @@
+export const SingleInstancePage = `
+// This code block is interactive - try changing it!
+() => (
+    <div className="grid grid-cols-2 gap-4">
+        <div className="col-span-2">
+            <h4>This is an example page </h4>
+            <p>With a sub-heading</p>
+        </div>
+        <div className="bg-primary text-primary rounded p-4">
+            This is a paragraph
+        </div>
+        <div className="bg-primary text-primary rounded p-4">
+           This is a paragraph
+        </div>
+    </div>
+)`;
